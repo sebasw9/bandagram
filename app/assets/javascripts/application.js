0e3e06e66fdc5b36532faddb21aaa46e94157247
Sebastian Walker <sebasw9@vt.edu>
2015-10-19 15:57:59 +0000

    Added some jquery and styled a bit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require bootstrap
+//= require masonry/jquery.masonry
 //= require turbolinks
 //= require_tree .
